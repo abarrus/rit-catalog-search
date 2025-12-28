5b2a29258a2df9d5a52bce4ef94bf5a6844af221
August Barrus <67875711+abarrus@users.noreply.github.com>
2025-12-28 11:15:46 -0500

    does nothing yet but working on search options

--- src/lib/js/consts.ts
+++ src/lib/js/consts.ts
@@ -101,3 +101,67 @@ export function filterfunc(item: CatalogItem) {
     })
     return ok;
 }
+
+enum SearchOption {
+    ALL = "All of the following",
+    NONE = "None of the following",
+    ANY = "Any (at least one) of the following",
+    ONE = "Just one of the following"
+};
+
+enum SingleSearchOption {
+    CONTAINS_TEXT = "Contains this text:",
+    HAS = "Has",
+    HASNT = "Doesn't have",
+    CREDIT_BTWN = "Credits between",
+    CREDIT_UNDER = "Credits below",
+    CREDIT_OVER = "Credits above"
+}
+
+class Option {
+    private type: SingleSearchOption;
+
+    constructor(type: SingleSearchOption) {
+        this.type = type;
+    }
+
+    // todo: this is nonsense atm.
+    check(item: CatalogItem) {
+        if (this.type == SingleSearchOption.HAS) {
+            return false;
+        }
+        return true;
+    }
+}
+
+class Container {
+    private type: SearchOption;
+    private opts: (Container|Option)[];
+
+    constructor(type: SearchOption) {
+        this.type = type;
+        this.opts = [];
+    }
+
+    add(opt: Container|Option) {
+        this.opts.push(opt);
+    }
+
+    remove(i: number) {
+        this.opts.splice(i, 1);
+    }
+
+    check(item: CatalogItem): boolean {
+        const matchLen = this.opts.filter(opt => opt.check(item)).length;
+        if (this.type == SearchOption.ALL) {
+            return matchLen == this.opts.length;
+        } else if (this.type == SearchOption.NONE) {
+            return matchLen == 0;
+        } else if (this.type == SearchOption.ANY) {
+            return matchLen >= 1;
+        } else {
+            // ONE
+            return matchLen == 1;
+        }
+    }
+}
