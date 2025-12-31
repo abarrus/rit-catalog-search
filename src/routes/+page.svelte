b65606b5a49674987528d45f0a8857c1617216dd
August Barrus <67875711+abarrus@users.noreply.github.com>
2025-12-31 11:36:15 -0500

    deleted all the things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,40 +3,25 @@
 
 
     // imports
-    import Nested from "./Nested.svelte";
-
     import type { CatalogItem } from "$lib/js/consts";
-    import { keys, catalog, options, choices, filterfunc, node, Container } from "$lib/js/consts";
+    import { keys, catalog, options, choices } from "$lib/js/consts";
 
     // empty states
     let results = $state<number>(0);
     let filteredCatalog : CatalogItem[] = $state<CatalogItem[]>([]);
-    let bigNode = $state<Container>(node);
+
+    function check(item: CatalogItem) {
+        
+    }
     
     function handleSubmit() {
         console.log("SEARCHINGGGG");
-        filteredCatalog = catalog.filter((item) => { return bigNode.check(item); });
+        filteredCatalog = catalog.filter((item) => { return check(item); });
         results = filteredCatalog.length;
         console.log(filteredCatalog.length)
     }
     handleSubmit();
 </script>
-<Nested node={bigNode} submit={handleSubmit}/>
-<!--
-<form onchange={handleSubmit}>
-    {#each keys as key}
-        <p>{key}</p>
-        <select multiple bind:value={choices[key]}>
-            {#each options[key] as item}
-                <option value={item}>
-                    {item}
-                </option>
-            {/each}
-        </select>
-    {/each}
-</form>
--->
-
 <h2>Results: {results}</h2>
 <div>
     <ol>
