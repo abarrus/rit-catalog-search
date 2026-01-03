adeeeda0debfcf9f42b26dd177f6e8e062910a13
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-01-03 13:01:54 -0500

    maybe github pages will work now?

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,11 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: "/rit-catalog-search"
+		},
+    	trailingSlash: 'always' // <- THIS IS THE KEY
 	}
 };
 
