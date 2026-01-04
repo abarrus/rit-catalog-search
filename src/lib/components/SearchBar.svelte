9e0419491777542b720ebb3371d3b9068e322f74
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-01-03 20:05:53 -0500

    pretty search bar (not functional)

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,4 +1,27 @@
-<div class="card">
-    <i class="bi bi-search"></i>
-    <input placeholder="Search...">
+<!--
+	Credit to Anonymous on bootstrapexamples.com 
+	https://bootstrapexamples.com/@anonymous/search-bar
+-->
+
+<style>
+	.search-container {
+		position: relative;
+	}
+
+	.search-input {
+		border-radius: 10px;
+		padding-left: 35px;
+	}
+
+	.search-icon {
+		position: absolute;
+		top: 50%;
+		left: 15px;
+		transform: translateY(-50%);
+		color: #888;
+	}
+</style>
+<div class="search-container m-3">
+    <input type="text" class="form-control search-input" placeholder="Search...">
+    <i class="bi bi-search search-icon"></i>
 </div>
