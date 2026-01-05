8397a59730725f76c8ff3bf06314864b2275a645
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-01-05 10:21:31 -0500

    this is supposed to make it so you can go to different pages and github pages doesnt get mad that it doesnt have a file for it

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-const dev = process.argv.includes('dev');
-
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        appDir: 'app', // Required as the default is _app
-        adapter: adapter(),
-		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
+  kit: {
+    appDir: "app", // Required as the default is _app
+    adapter: adapter({
+      fallback: "index.html", // this is supposed to make it so you can go to different pages and github pages doesnt get mad that it doesnt have a file for it
+    }),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
     },
-    preprocess: vitePreprocess()
+  },
+  preprocess: vitePreprocess(),
 };
 export default config;
